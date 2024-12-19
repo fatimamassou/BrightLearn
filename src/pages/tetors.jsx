8668e9247d4285bf293e35d8f63a0c569f1275c7
fatmamath <massoufatima23@gmail.com>
2024-12-20 00:47:02 +0100

    update

--- src/pages/tetors.jsx
+++ src/pages/tetors.jsx
@@ -28,7 +28,7 @@ function Tetors() {
         }  , 
         Price :{
             identifier: 'number.int',
-            args: {min: 2, max: 14, multipleOf: 1}
+            args: {min: 2, max: 20, multipleOf: 1}
         }  ,       
         
     } , 4);
