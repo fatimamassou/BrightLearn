62f2b9b193b3411fe2991c1086c4a4559d7083c4
fatmamath <massoufatima23@gmail.com>
2024-12-21 23:19:31 +0100

    Contact section is done.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -10,27 +10,26 @@ import Tiktok from "../pictures/Purple aesthetic icon TikTok.jpeg";
 
 function Contact() {
     return(
-        <div className="h-screen bg-bg_secondary flex items-center justify-center space-x-12">
-            <div className="max-w-[500px] p-6">
+        <div className="h-screen bg-bg_secondary flex items-center justify-center py-10 space-x-12">
+            <div className="max-w-[540px] p-6 flex-grow">
                 <form action="submitedform" className="space-y-4">
-                    
-                    <div className="">
+                    <div>
                         <Lable>Full Name</Lable>
                         <Input type="text" Placeholder="Full Name" Id="" Name="" />
                     </div>
 
-                    <div className="">
+                    <div>
                         <Lable>Your Email</Lable>
                         <Input type="email" Placeholder="Email Address" Id="" Name="" />
                     </div>
                     
-                    <div className="">
+                    <div>
                         <Lable>Subject</Lable>
                         <Input type="text" Placeholder="Subject" Id="" Name="" />
                     </div>
                     
-                    <div className="">
-                        <Lable>Full Name</Lable>
+                    <div>
+                        <Lable>Message</Lable>
                         <Input type="textarea" Placeholder="Your Message ....." Id="" Name="" />                
                     </div>
                     
@@ -39,7 +38,8 @@ function Contact() {
                     </button>
                 </form>
             </div>
-            <div className="space-y-8 max-w-[540px]">
+
+            <div className="space-y-6 max-w-[540px] pb-16">
                 <h1 className="text-primary-200 text-4xl ">Contact Us</h1>
                 <div className="text-secondary-200 space-y-2">
                     <h1 className="text-5xl">Hit Us Up!</h1>
@@ -52,34 +52,33 @@ function Contact() {
 
                     <div className="flex space-x-6">
                         <img src={Phone} alt="Phone" className="w-16 rounded-lg"/>
-                        <div className="space-y-2">
-                            <p className="text-xl font-medium">Call Us</p>
+                        <div className="space-y-1">
+                            <p className="text-lg font-medium">Call Us</p>
                             <p>+212-645-678-890</p>
                         </div>
                     </div>
 
                     <div className="flex space-x-6">
                         <img src={Gmail} alt="Gmail" className="w-16 rounded-lg"/>
-                        <div className="space-y-2">
-                            <p className="text-xl font-medium">Email US</p>
+                        <div className="space-y-1">
+                            <p className="text-lg font-medium">Email Us</p>
                             <p>BrightLearn@gmail.com</p>
                         </div>
                     </div>
-                
                 </div>
 
-                <div className="block space-y-4">
-                    <h1>Follow Us !</h1>
+                <div className="block space-y-2 text-lg font-medium">
+                    <h1>Follow Us!</h1>
                     <div className="flex space-x-2">
-                        <img src={Insta} alt="Gmail" className="w-8 rounded-lg"/>
-                        <img src={Tiktok} alt="Tiktok" className="w-8 rounded-lg"/>
-                        <img src={Youtube} alt="Youtube" className="w-8 rounded-lg"/>
+                        <img src={Insta} alt="Instagram" className="w-8 rounded-lg"/>
+                        <img src={Tiktok} alt="TikTok" className="w-8 rounded-lg"/>
+                        <img src={Youtube} alt="YouTube" className="w-8 rounded-lg"/>
                         <img src={Pinterest} alt="Pinterest" className="w-8 rounded-lg"/>
                     </div>
                 </div>
-
             </div>
-        </div>    
+        </div>
+
     );
 }
 
