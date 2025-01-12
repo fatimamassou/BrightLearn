4886162dc5f6384eb060da29202e0b0661c1afe2
fatmamath <massoufatima23@gmail.com>
2025-01-12 23:50:15 +0100

    Add SingUpTeacher page and create select component

---
+++ src/components/select.jsx
@@ -0,0 +1,22 @@
+import { subjects } from '../data' ;
+
+function Select ({onChange, selectedSubject}) {
+    const classStyle = "w-full border-2 border-primary-200 rounded-xl outline-0 py-2 px-4 text-primary-100 font-normal bg-transparent";
+    
+    return(
+        <select 
+        id="celectSubject" 
+        name="celectSubject" 
+        className={classStyle}
+        onChange={onChange}
+        value={selectedSubject}
+        >
+            {subjects.map((element)=> (
+            <option value={element} key={element}>
+                {element}
+            </option>))}
+        </select>
+    );
+}
+
+export default Select ;
--- src/pages/SingUpTeacher.jsx
+++ src/pages/SingUpTeacher.jsx
@@ -1,7 +1,60 @@
-
+import Input from '../components/inputs';
+import Lable from '../components/lable';
+import teacherImg from '../pictures/glory.jpeg';
+import { Link } from 'react-router-dom';
 function SingUpTeacher() {
     return(
-        <></>
+        <div className="w-full lg:h-screen flex bg-secondary">
+            <div className="lg:w-1/2 flex items-center justify-center">
+                <div className="w-2/3 space-y-6">
+                    <h1 className="text-4xl text-primary-100 text-center">Sign Up</h1>
+                    <form action="submitedform" className="space-y-12">
+                      <div className="space-y-4">
+                            <div>
+                                <Lable>Full Name</Lable>
+                                <Input type="text" Placeholder="Full Name" Id="" Name="" />
+                            </div>
+
+                            <div>
+                                <Lable>Your Email</Lable>
+                                <Input type="email" Placeholder="Email Address" Id="" Name="" />
+                            </div>
+                            
+                            <div>
+                                <Lable>Your Phone</Lable>
+                                <Input type="tel" Placeholder="Phone number" Id="" Name="" />
+                            </div>
+                            
+                            <div>
+                                <Lable>Full Name</Lable>
+                                <Input type="password" Placeholder="Full Name" Id="" Name="" />
+                            </div>
+                            
+                            <div>
+                                <Lable>Full Name</Lable>
+                                <Input type="password" Placeholder="Full Name" Id="" Name="" />
+                            </div>
+                        </div>
+                        <div className="space-y-6">
+                            <Link 
+                            to='/singin'
+                            >
+                               <button className="bg-primary-100 text-white text-xl py-3 rounded-lg w-full ">Jump In!</button> 
+                            </Link>
+                            <div className="h-[1px] w-full bg-primary-200"></div>
+                            <div className="text-center text-primary">Already in the club? Jump to 
+                                <Link to='/singin'>
+                                    <span className='text-primary-200 font-semibold underline underline-offset-2 '> Log In!</span> 
+                                </Link>
+                                </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className="lg:w-1/2">
+                <img src={teacherImg} alt="studentImg" className='w-full h-full'/>
+            </div>
+        </div>
     );
 };
 
