712cc977c28805bbbf62ecf34d2858d52e3aac68
fatmamath <massoufatima23@gmail.com>
2025-01-15 23:54:22 +0100

    add layouts folder and layout has nav bar and footer and use it for see all page.

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,19 @@
+import NavBar from "../components/navBar";
+import Footer from "../pages/footer";
+
+function Layout({children}) {
+    return(
+            <div className="bg-bg_secondary w-full">
+                <div className="fixed top-0 left-0 w-full z-50">
+                    <NavBar bgColor="bg-secondary"/>
+                </div>
+                <div className="pt-36 p-10">
+                    {children}
+                </div>
+                <Footer/>
+            </div>
+
+    );
+}
+
+export default Layout ;
--- src/pages/SeeAll.jsx
+++ src/pages/SeeAll.jsx
@@ -1,8 +1,7 @@
-import NavBar from "../components/navBar";
 import Tetors from "../components/TetorsComponent";
-import Footer from "./footer";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import Layout from "../layouts/Layout";
 
 function SeeAll() {
     const location = useLocation();
@@ -11,15 +10,9 @@ function SeeAll() {
     },[location])
     return(
         <section id="seeAll" >
-            <div className="bg-bg_secondary w-full">
-                <div className="fixed top-0 left-0 w-full z-50">
-                    <NavBar bgColor="bg-secondary"/>
-                </div>
-                <div className="pt-36 p-10">
-                    <Tetors/>
-                </div>
-                <Footer/>
-            </div>
+           <Layout>
+                <Tetors/>     
+           </Layout>
         </section>
 
     );
