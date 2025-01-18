5c379dee76561c6f0d52abe385db94fbb5fd9703
fatmamath <massoufatima23@gmail.com>
2025-01-19 00:01:45 +0100

    Add all option in category and remove the background in navBar.

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -5,7 +5,7 @@ function Layout({children}) {
     return(
             <div className="bg-bg_secondary w-full">
                 <div className="fixed top-0 left-0 w-full z-50">
-                    <NavBar bgColor="bg-secondary"/>
+                    <NavBar />
                 </div>
                 <div className="pt-36 p-10">
                     {children}
