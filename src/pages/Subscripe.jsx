554f51cb69eefd3c9d78dd11f6aad959ff8dc53e
fatmamath <massoufatima23@gmail.com>
2025-01-22 21:55:46 +0100

    subscripe design.

--- src/pages/Subscripe.jsx
+++ src/pages/Subscripe.jsx
@@ -10,12 +10,14 @@ function SubscriptionPlans () {
         </h1>
         <div className="flex justify-center space-x-6">
           {/* Monthly Plan */}
-          <div className="bg-grey h-[500px] shadow-md rounded-100 p-6 w-[360px]">
+          <div className="relative bg-grey h-[500px] shadow-md rounded-3xl p-6 w-[360px] space-y-14">
+           <div className="">
             <h2 className="text-2xl font-semibold mb-4">Monthly</h2>
-            <p className="mb-4">
-              Students and teachers seeking on-demand access to learning
-              resources.
-            </p>
+              <p className="mb-4">
+                Students and teachers seeking on-demand access to learning
+                resources.
+              </p>
+           </div>
             <div className="text-4xl font-semibold mb-6"><span className="text-lg font-normal">$</span>9.99<span className="text-lg font-normal">/mo</span></div>
             <ul className="text-left mb-6 space-y-2">
               <li><TaskAltIcon /> Access to recorded lessons</li>
@@ -23,18 +25,20 @@ function SubscriptionPlans () {
               <li><TaskAltIcon /> Access to discussion forums</li>
               <li><TaskAltIcon /> Monthly progress tracking</li>
             </ul>
-            <button className="w-full bg-purple-600 text-white py-2 rounded-lg hover:bg-purple-700 transition">
+            <button className="w-full bg-primary-100 text-xl text-white py-2 rounded-b-3xl hover:bg-purple-700 transition absolute bottom-0 left-0 ">
               Choose Monthly Plan
             </button>
           </div>
 
           {/* Yearly Plan */}
-          <div className="bg-grey h-[5O0px] shadow-md rounded-100 w-[360px] relative">
-            <div className="px-6 pt-6">
+          <div className="relative bg-grey h-[520px] shadow-md rounded-3xl w-[360px]">
+            <div className="px-6 pt-6 space-y-10">
+             <div className="">
               <h2 className="text-2xl font-semibold mb-4">Yearly</h2>
-              <p className="mb-4">
-                Students and teachers seeking year-round support and resources.
-              </p>
+                <p className="mb-4">
+                  Students and teachers seeking year-round support and resources.
+                </p>
+             </div> 
               <div className="text-4xl font-semibold mb-6"><span className="text-lg font-normal">$</span>109.99<span className="text-lg font-normal">/mo</span></div>
               <ul className="text-left mb-6 space-y-2">
                 <li><TaskAltIcon /> Everything in Monthly, plus:</li>
@@ -44,7 +48,7 @@ function SubscriptionPlans () {
                 <li><TaskAltIcon /> Personalized communication with instructors</li>
               </ul>
             </div>
-            <button className="w-full bg-primary-100 text-white py-4 rounded-xl hover:bg-purple-700 transition absolute bottom-0 left-0 ">
+            <button className="w-full bg-primary-100 text-xl text-white py-2 rounded-b-3xl hover:bg-purple-700 transition absolute bottom-0 left-0 ">
               Choose Yearly Plan
             </button>
           </div>
