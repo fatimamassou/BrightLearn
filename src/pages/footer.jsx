7f351b62f2b8da18da4329c7420f3cd970ae7a33
fatmamath <massoufatima23@gmail.com>
2025-01-24 22:59:31 +0100

    add animation to socail icons in footer.

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -4,6 +4,7 @@ import Pinterest from '../pictures/Pinterest.jpeg';
 import Tiktok from '../pictures/Purple aesthetic icon TikTok.jpeg';
 
 function Footer() {
+  const socailIconStyle = 'rounded-lg hover:shadow-300 transition-all duration-500';
   return(
     <div className="footer text-primary-100 font-semibold w-full py-8 flex items-center justify-center">
       <div className="space-y-2">
@@ -11,16 +12,16 @@ function Footer() {
         <p className="text-center">Have any questions? Need help? Contact Us anytime!</p>
         <p className="text-center">@ 2024 BrightLearn. All rights reserved.</p>
         <div className="flex items-center justify-center space-x-2 ">
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className={socailIconStyle}>
             <img src={Insta} alt="Instagram" className="w-8 rounded-lg" />
           </a>
-          <a href="https://www.tiktok.com" target="_blank"  rel="noopener noreferrer">
+          <a href="https://www.tiktok.com" target="_blank"  rel="noopener noreferrer" className={socailIconStyle}>
             <img src={Tiktok} alt="TikTok" className="w-8 rounded-lg" />
           </a>
-          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className={socailIconStyle}>
             <img src={Youtube} alt="YouTube" className="w-8 rounded-lg" />
           </a>
-          <a href="https://www.pinterest.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.pinterest.com" target="_blank" rel="noopener noreferrer" className={socailIconStyle}>
             <img src={Pinterest} alt="Pinterest" className="w-8 rounded-lg" />
           </a>
         </div>
