18822d61f006756b404bb9a74be67848591c8b57
fatmamath <massoufatima23@gmail.com>
2025-01-24 22:45:13 +0100

    style button.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -34,7 +34,11 @@ function Contact() {
               <Input type="textarea" Placeholder="Your Message ....." />                
             </div>
                         
-            <button type="submit" className="bg-primary text-white text-xl px-12 py-2 rounded-lg hover:shadow-300 transition-all duration-700">
+            <button 
+            type="submit" 
+            className="bg-primary text-white text-xl px-12 py-2 rounded-lg 
+                      hover:shadow-300 transition-all duration-700"
+            >
                             Send 
             </button>
           </form>
