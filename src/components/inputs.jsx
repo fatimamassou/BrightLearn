49af16dfc1a0759dd33aba798150e9ea8d6f2921
fatmamath <massoufatima23@gmail.com>
2025-01-25 15:12:43 +0100

    add functions for add teacher and cancel selection.

--- src/components/inputs.jsx
+++ src/components/inputs.jsx
@@ -1,5 +1,5 @@
 
-function Input({ type, Placeholder }) {
+function Input({ type, Placeholder, value, onClick }) {
   const classStyle = 'w-full border-2 border-primary-200 rounded-xl outline-0 py-2 px-4 '+
                       'text-primary-100 font-normal bg-transparent';
   return(            
@@ -8,6 +8,8 @@ function Input({ type, Placeholder }) {
       pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" 
       placeholder="123-456-7890" 
       className={classStyle}
+      onClick={onClick}
+      value={value}
       required
     />
       : type === 'textarea' ? 
@@ -18,13 +20,17 @@ function Input({ type, Placeholder }) {
           rows="4" 
           placeholder={Placeholder} 
           wrap="soft" 
-          className={classStyle} 
+          className={classStyle}
+          onClick={onClick}
+          value={value} 
           required 
         />
         :<input 
           type={type}
           className={classStyle}
           placeholder={Placeholder}
+          onClick={onClick}
+          value={value}
           required
         />
   );
