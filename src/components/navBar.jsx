f8fe5ef4dffc3a71fa85b52cf6d3ac1ff27f0c6c
fatmamath <massoufatima23@gmail.com>
2025-01-29 23:31:29 +0100

    rename img to assets

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,8 +1,6 @@
 import{ useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import logo from '../pictures/logo.png';
-
 import Button from './buttons';
 import NavBarLinks from './NavBarLinks';
 
