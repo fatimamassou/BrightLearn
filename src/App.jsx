f706d5fb5ce9c16276d28777e70ee09e9a20a751
fatmamath <massoufatima23@gmail.com>
2025-01-31 20:41:36 +0100

    Add style to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import LogIn from './pages/Login';
-import Register from './pages/Register';
-import SeeAll from './pages/SeeAll';
-import SeeMore from './pages/SeeMore';
 import SingUpStudent from './pages/SingUpStudent';
 import SingUpTeacher from './pages/SingUpTeacher';
 import SubscriptionPlans from './pages/Subscribe';
+import LandingPage from './pages/LandingPage';
+import Register from './pages/Register';
+import SeeMore from './pages/SeeMore';
+import SeeAll from './pages/SeeAll';
+import LogIn from './pages/Login';
 import './styles/App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LogIn />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/seeAll" element={<SeeAll />} />
+        <Route path="/seemore" element={<SeeMore />} /> 
         <Route path="/register" element={<Register />} />
         <Route path="/singupstudent" element={<SingUpStudent />} />
         <Route path="/singupteacher" element={<SingUpTeacher />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/seemore" element={<SeeMore />} /> 
         <Route path="/subscripe" element={<SubscriptionPlans />} />
       </Routes>
     </BrowserRouter>
